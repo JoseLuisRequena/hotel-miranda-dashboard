82a065140f98166bd45897e4b8bc05ff16e064f0
Jose Luis Estefania <jlrequenaetcheverria@gmail.com>
2022-08-26 08:43:30 +0200

    change in Slices

--- src/Slices/bookingsSlice.js
+++ src/Slices/bookingsSlice.js
@@ -6,7 +6,18 @@ export const getAllBookings = createAsyncThunk(
     'bookings/getAllBooking',
     async () => {
         return new Promise((resolve) => 
-        setTimeout(resolve(ArrayBookings), 0));
+            setTimeout(resolve(ArrayBookings), 0)
+        );
+    }
+)
+
+export const getBooking = createAsyncThunk(
+    'bookings/booking',
+    async (id) => {
+        return new Promise( resolve => 
+            setTimeout(resolve(ArrayBookings.filter(item => item.id === id)), 
+            0)
+        )
     }
 )
 
@@ -19,16 +30,29 @@ export const bookingsSlice = createSlice({
     reducers: {
         addBookings: (state, action) => {
             return void state.allBookings.push(action.payload);
-        }
+        },
+        editBooking: (state, action) => {
+            return state.map((booking) =>
+              booking.id === action.payload.id ? action.payload : booking
+            );
+        },
+        deleteBooking: (state, action) => {
+            console.log(state)
+            return state.filter((booking) => booking.id !== action.payload.id);
+        },
     },
     extraReducers: (builder) => {
-       
-        builder.addCase(getAllBookings.fulfilled, (state, action) => {
+        builder
+        .addCase(getAllBookings.fulfilled, (state, action) => {
             return void (state.allBookings = action.payload);
         })
+        .addCase(getBooking.fulfilled, (state, action) => {
+            console.log(action.payload)
+            //return void (state.booking = action.payload);
+        });
   }})
   
-  export const { addBookings } = bookingsSlice.actions
+  export const { addBookings, editBooking, deleteBooking } = bookingsSlice.actions
 
   export const allBookingsArray = (state) => state.bookings.allBookings
   export const oneBooking = (state) => state.bookings.booking
