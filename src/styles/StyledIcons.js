9a168cc040091c58fdc4a847fc76c86be7e623ab
unknown <jlrequenaetcheverria@gmail.com>
2023-01-09 17:06:11 +0100

    refactor

--- src/styles/StyledIcons.js
+++ src/styles/StyledIcons.js
@@ -4,22 +4,6 @@ import { ReactComponent as BellIcon } from "../imgs/iconBell.svg";
 import { ReactComponent as EnvelopeIcon } from "../imgs/iconEnvelope.svg"
 import { ReactComponent as ArrowsIcon } from "../imgs/iconArrows.svg"
 
-
-
-export const StyledHeader = styled.div`
-    display:flex;
-    justify-content: space-between;
-    top: 0;
-    width: 100%;
-    height: 120px;
-    font-size: 24px;
-    line-height: 60px;
-    text-align: center;
-    margin-bottom: 30px;
-    background: #FFF;
-    box-shadow: 13px 3px 40px #00000010;
-`
-
 const Icon = styled.i`
   min-width: 48px;
   height: 48px;
